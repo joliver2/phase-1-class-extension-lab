1230b9d93e8b7167d6f186d5c84dc14619487cbb
joliver427@icloud.com <joliver427@icloud.com>
2023-01-18 20:13:59 -0700

    commit tree 6bc114e14ea8ed2f633a65557a432e37ee966c41

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+class Polygon {
+    constructor (array) {
+        this.array = array
+    }
+    get countSides() {
+        return this.array.length
+    }
+    get perimeter() {
+        const sum = this.array.reduce((a, b) => a+b);
+        return sum
+    }
+
+}   
+
+class Triangle extends Polygon {
+    get isValid() {
+        return this.array[0] < (this.array[1]+this.array[2]) 
+        && this.array[1] < (this.array[0]+this.array[2]) 
+        && this.array[2] < (this.array[0]+this.array[1]);
+    }
+}
+
+class Square extends Polygon{
+    get isValid() {
+        return this.array[0] === this.array[1]
+        && this.array[1] === this.array[2] 
+        && this.array[2] === this.array[3]
+    }
+    get area() {
+        return this.array[0]*this.array[1]
+    }
+}
